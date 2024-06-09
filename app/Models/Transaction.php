8a163775618d302ab521d9f692ba97989e146b90
Sujan Molla <smitexpert@gmail.com>
2024-06-09 23:40:16 +0600

    UPDATE | Transaction Model add user relation

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -23,4 +23,9 @@ class Transaction extends Model
     public function wallet() {
         return $this->belongsTo(Wallet::class);
     }
+
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
